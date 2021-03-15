72c2e5e5b1dc08b254dfe39e0f564ba0b9c757ca
Priyanka24Singh <65005167+Priyanka24Singh@users.noreply.github.com>
2021-03-15 18:25:22 +0530

    Add files via upload

---
+++ assets/js/timer.js
@@ -0,0 +1,21 @@
+var countDown = new Date("Jan 5,2020 13:00:00").getTime();
+var x = setInterval(function(){
+
+
+
+
+var now = new Date().getTime();
+var distance = countDown - now;
+//var days = Math.floor(distance/(1000*60*60*24));
+var hours = Math.floor((distance % (1000*60*60*24))/(1000*60*60));
+var mins = Math.floor((distance % (1000*60*60))/(1000*60));
+var sec = Math.floor((distance % (1000*60))/(1000));
+
+document.getElementById("timer").innerHTML = "Time Left: "+hours+"h"+mins+"m"+sec+"s";
+
+if(distance<0){
+	clearInterval(x);
+	document.getElementById("timer").innerHTML="Time UP";
+}
+
+},1000);
